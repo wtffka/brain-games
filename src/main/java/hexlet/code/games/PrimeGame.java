38f37f6dfff31b4e8b40af0e693bf7115d09756d
VYATCHESLAV BALAKHONOV <slaviklal@yandex.ru>
2022-01-30 00:38:29 +0300

    bug fixed

--- src/main/java/hexlet/code/games/PrimeGame.java
+++ src/main/java/hexlet/code/games/PrimeGame.java
@@ -2,22 +2,22 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class PrimeGame {
-    static final int MAX_VALUE = 5;
+    static final int MAX_VALUE = 10;
     static final int MAX_RIGHT_ANSWERS = 3;
     static final String GAME_RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
     public static Map<String, String> generateData() {
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new LinkedHashMap<>();
         for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
             int a = Utils.newRandomNumber(MAX_VALUE);
             String key = String.valueOf(a);
             key = changeKey(map, key);
-            map.put(key, divisors(a) > 0 ? "no" : "yes");
+            map.put(key, divisors(key) > 0 ? "no" : "yes");
         }
         return map;
     }
@@ -26,10 +26,11 @@ public class PrimeGame {
         Engine.startGame(generateData(), Engine.getName(s), s, GAME_RULES);
     }
 
-    public static int divisors(int a) {
+    public static int divisors(String a) {
+        int intA = Integer.parseInt(a);
         var divisors = 0;
-        for (int i = 2; i < a; i++) {
-            if (a % i == 0) {
+        for (int i = 2; i < intA; i++) {
+            if (intA % i == 0) {
                 divisors++;
             }
         }
